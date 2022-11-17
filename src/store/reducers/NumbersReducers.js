779c61d3b95f7038f566f8d4f8f4e8ff1e775a71
Pedro <pedro7109.j@gmail.com>
2022-11-17 11:20:52 -0300

    Constants for Action Types

--- src/store/reducers/NumbersReducers.js
+++ src/store/reducers/NumbersReducers.js
@@ -1,3 +1,8 @@
+import {
+    NUM_MIN_EDIT,
+    NUM_MAX_EDIT
+} from "../actions/ActionsTypes"
+
  const initialState = {
     min:1,
     max:10
@@ -6,13 +11,13 @@
  export default function(state = initialState, action) {
        
     switch(action.type){
-        case 'NUM_MIN_EDIT':
+        case NUM_MIN_EDIT:
             return {
                 ...state,
                 min : action.payload
             }
 
-            case 'NUM_MAX_EDIT':
+            case NUM_MAX_EDIT:
             return {
                 ...state,
                 max : action.payload
