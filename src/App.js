528b6f560ca344ee6dfd32bd0f20b8fa4f10e90e
Pedro <pedro7109.j@gmail.com>
2022-11-17 09:03:22 -0300

    Implementation without Redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+import Interval from './components/Interval';
 import Media from './components/Media';
 import Sorteio from './components/Sorteio';
 import Soma from './components/Soma';
 
-import Interval from './components/Interval';
+
 
 function App() {
+
+  const [min, setMin] = useState(10)
+  const [max, setMax] = useState(20)
+
   return (
     <div className="App">
       <h1>Exercicios React-Redux</h1>
       <div className="linha">
-     <Interval></Interval>
+     <Interval min={min} max={max} 
+     onMinChanged={setMin} onMaxChanged={setMax}></Interval>
      </div>
      <div className="linha">
-     <Media></Media>
-     <Soma></Soma>
-     <Sorteio></Sorteio>
+     <Media min={min} max={max}></Media>
+     <Soma min={min} max={max}></Soma>
+     <Sorteio min={min} max={max}></Sorteio>
      </div>
     </div>
   );
